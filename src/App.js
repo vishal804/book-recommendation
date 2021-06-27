71e599cb38e48e51c8918c749020eac42ff38dab
vishal804 <vishal804>
2021-06-27 14:49:45 +0530

    Book database added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { bookDB } from "./bookdatabase";
+import "./styles.css";
 
-function App() {
+const book = Object.keys(bookDB);
+
+export default function App() {
+  const [genre, setGenre] = useState("fiction");
+
+  function clickGenre(genreKey) {
+    setGenre(genreKey);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="heading"> 📚 Bestbooks</h1>
+      <h2 id="subheading">
+        Checkout my favorite books. Select a genre to get started
+      </h2>
+      <hr></hr>
+
+      <div id="booklist">
+        {book.map((genre) => {
+          return (
+            <button id="btn" key={genre} onClick={() => clickGenre(genre)}>
+              {genre}
+            </button>
+          );
+        })}
+      </div>
+      <div>
+        <ul>
+          {bookDB[genre].map((book) => {
+            return (
+              <li className="list" key={book.name}>
+                <div id="name">{book.name}</div>
+                <div id="rating">{book.rating}</div>
+                <div id="author">{book.author}</div>
+                <br />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <footer id="footer">
+        <h5>© All rights reserved 2021</h5>
+      </footer>
     </div>
   );
 }
-
-export default App;
---
+++ src/bookdatabase.js
@@ -0,0 +1,88 @@
+export const bookDB = {
+    fiction: [
+      { 
+        name: "The Alchemist", 
+        rating: "4.7/5", 
+        author: "by Paulo Coelho" 
+      },
+      {
+        name: "Shiva Trilogy",
+        rating: "5/5",
+        author: "by Amish Tripathi"
+      },
+      {
+        name: "Harry Potter and the Chamber of Secrets",
+        rating: "4.43/5",
+        author: "by J.K. Rowling, Mary GrandPré"
+      },
+      {
+        name: "2 States: The Story of My Marriage",
+        rating: "3.43/5",
+        author: "by Chetan Bhagat"
+      },
+      {
+        name:
+          "The Water Phoenix: A Memoir of Childhood Abuse, Healing and Forgiveness",
+        rating: "3.5/5",
+        author: "by Rituparna Chatterjee"
+      }
+    ],
+    business: [
+      {
+        name: "Dhirubhai Ambani: Against All Odds",
+        rating: "4.2/5",
+        author: " by Krishnamurthy"
+      },
+      {
+        name: "Never Split the Difference",
+        rating: "3.5/5",
+        author: " by Christopher Voss and Tahl Raz"
+      },
+      {
+        name: "Dream with your open eyes",
+        rating: "3.9/5",
+        author: "by Ronnie Screwvala"
+      },
+      {
+        name: "Loonshots",
+        rating: "4.6/5",
+        author: "by Safi Bahcall"
+      }
+    ],
+    biography: [
+      {
+        name: "Wings of Fire",
+        rating: "4.8/5",
+        author: "by Arun Tiwari and Dr. A. P. J. Abdul Kalam"
+      },
+      {
+        name: "An Autobiography: The story of My Experiments with Truth",
+        rating: "4.3/5",
+        author: "by Mahatma Gandhi"
+      },
+      {
+        name: "Mind Master: Winning Lessons from a Champion's Life",
+        rating: "4.4/5",
+        author: "by Susan Ninan and Viswanathan Anand"
+      },
+      {
+        name: "Playing It My Way",
+        rating: "4/5",
+        author: "by Boria Majumdar and Sachin Tendulkar"
+      }
+    ],
+    javascript: [
+      {
+        name: "Eloquent JavaScript: A Modern Introduction to Programming",
+        rating: "4/5",
+        author: "by Marijn Haverbeke"
+      },
+      { name: "You Don't Know JS", rating: "4.9/5", author: "by Kyle Simpson" },
+      {
+        name: "JavaScript: The Good Parts",
+        rating: "4.4",
+        author: "Douglas Crockford"
+      }
+    ]
+  };
+  
